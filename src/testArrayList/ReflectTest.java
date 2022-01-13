ef12a57320d82867ac01633317981898afddf224
Michael-cybersecurity <xjq20011023@163.com>
2022-01-13 15:45:24 +0800

    学习反射机制

--- src/testArrayList/ReflectTest.java
+++ src/testArrayList/ReflectTest.java
@@ -5,7 +5,9 @@ package testArrayList;
  *
  * @author 许 劲淇
  * @date 2022/01/13 14:42
- **/
+ */
+
+import testAbstract.Student;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -20,17 +22,20 @@ public class ReflectTest {
         Scanner scanner = new Scanner(System.in);
         System.out.println("输入类名：");
         name = scanner.next();
+//        Student p = new Student("Harry", "Science");
 
         //打印类名和超类名
         Class<?> cl = Class.forName(name);
+//        name = p.getName();
+//        Class<?> cl = p.getClass();
         Class<?> supercl = cl.getSuperclass();
 
         //打印访问类型
         String modifiers = Modifier.toString(cl.getModifiers());
         if (modifiers.length() > 0) {
-            System.out.println(modifiers + " ");
+            System.out.print(modifiers + " ");
         }
-        System.out.println("class " + name);
+        System.out.print("class " + name);
         if (supercl != null && supercl != Object.class) {
             System.out.println(" extends " + supercl.getName());
         }
@@ -49,12 +54,12 @@ public class ReflectTest {
      *
      * @param cl a class
      */
-    public static void printConstructors(Class cl) {
-        Constructor[] constructors = cl.getDeclaredConstructors();
+    public static void printConstructors(Class<?> cl) {
+        Constructor<?>[] constructors = cl.getDeclaredConstructors();
 
         System.out.println("    //构造方法");
 
-        for (Constructor c : constructors) {
+        for (Constructor<?> c : constructors) {
             //输出访问类型和方法名
             String name = c.getName();
             System.out.print("    ");
@@ -65,7 +70,7 @@ public class ReflectTest {
             System.out.print(name + "(");
 
             //输出参数
-            Class[] parameterTypes = c.getParameterTypes();
+            Class<?>[] parameterTypes = c.getParameterTypes();
             for (int i = 0; i < parameterTypes.length; i++) {
                 if (i > 0) {
                     System.out.print(", ");
@@ -79,14 +84,14 @@ public class ReflectTest {
     /**
      * 输出方法名
      *
-     * @param cl
+     * @param cl a class
      */
-    public static void printMethods(Class cl) {
+    public static void printMethods(Class<?> cl) {
         Method[] methods = cl.getDeclaredMethods();
         System.out.println("    //成员方法");
 
         for (Method m : methods) {
-            Class returnType = m.getReturnType();
+            Class<?> returnType = m.getReturnType();
             String name = m.getName();
 
             System.out.print("    ");
@@ -97,7 +102,7 @@ public class ReflectTest {
             System.out.print(returnType.getName() + " " + name + "(");
 
             //打印输出参数
-            Class[] parameterTypes = m.getParameterTypes();
+            Class<?>[] parameterTypes = m.getParameterTypes();
             for (int i = 0; i < parameterTypes.length; i++) {
                 if (i > 1) {
                     System.out.print(", ");
@@ -111,14 +116,14 @@ public class ReflectTest {
     /**
      * 输出声明参数
      *
-     * @param cl
+     * @param cl a class
      */
-    public static void printFields(Class cl) {
+    public static void printFields(Class<?> cl) {
         Field[] fields = cl.getDeclaredFields();
         System.out.println("    //变量和常量");
 
         for (Field field : fields) {
-            Class type = field.getType();
+            Class<?> type = field.getType();
             String name = field.getName();
             System.out.print("    ");
             String modifiers = Modifier.toString(field.getModifiers());
